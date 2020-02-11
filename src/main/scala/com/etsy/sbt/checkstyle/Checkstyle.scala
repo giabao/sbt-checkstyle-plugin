4c5289d35f8076e2e70f59dae5bacbcba0bbe547
Bùi Việt Thành <thanhbv@sandinh.net>
2020-02-11 17:51:24 +0700

    re-impl checkstyle task

--- src/main/scala/com/etsy/sbt/checkstyle/Checkstyle.scala
+++ src/main/scala/com/etsy/sbt/checkstyle/Checkstyle.scala
@@ -1,12 +1,12 @@
 package com.etsy.sbt.checkstyle
 
 import javax.xml.transform.stream.StreamSource
-
 import com.etsy.sbt.checkstyle.CheckstyleSeverityLevel._
-import com.puppycrawl.tools.checkstyle.Main.{main => CheckstyleMain}
 import net.sf.saxon.s9api.Processor
-import sbt.Keys._
 import sbt._
+import sbt.util.Logger
+
+import scala.xml.XML
 
 /**
   * A Scala wrapper around the Checkstyle Java API
@@ -16,85 +16,26 @@ import sbt._
   */
 object Checkstyle {
   /**
-    * Runs checkstyle
-    *
-    * @param javaSource The Java source path.
-    * @param outputFile The Checkstyle report output path.
-    * @param configLocation The Checkstyle config location.
-    * @param xsltTransformations XSLT transformations to apply.
-    * @param severityLevel The severity level used to fail the build.
-    */
-  def checkstyle(javaSource: File,
-                 outputFile: File,
-                 headerFile: File,
-                 configLocation: String,
-                 xsltTransformations: Option[Set[CheckstyleXSLTSettings]],
-                 severityLevel: Option[CheckstyleSeverityLevel],
-                 streams: TaskStreams): Unit = {
-    val outputLocation = outputFile.absolutePath
-
-    if (headerFile.getName != "") {
-      if (headerFile.exists()) {
-        sys.props("checkstyle.header.file") = headerFile.getAbsolutePath
-      } else {
-        streams.log.warn(s"checkstyleHeaderLocation file not found: $headerFile")
-      }
-    }
-
-    val checkstyleArgs = Array(
-      "-c", configLocation, // checkstyle configuration file
-      javaSource.absolutePath, // location of Java source file
-      "-f", "xml", // output format
-      "-o", outputLocation // output file
-    )
-
-    // Checkstyle calls System.exit which would exit SBT
-    // Thus we wrap the call to it with a special security policy
-    // that forbids exiting the JVM
-    noExit {
-      CheckstyleMain(checkstyleArgs: _*)
-    }
-
-    xsltTransformations match {
-      case None => // Nothing to do
-      case Some(xslt) => applyXSLT(file(outputLocation), xslt)
-    }
-
-    if (file(outputLocation).exists && severityLevel.isDefined) {
-      val log = streams.log
-      val issuesFound = processIssues(log, outputLocation, severityLevel.get)
-
-      if (issuesFound > 0) {
-        log.error(issuesFound + " issue(s) found in Checkstyle report: " + outputLocation + "")
-        sys.exit(1)
-      }
-    }
-  }
-
-  /**
     * Processes style issues found by Checkstyle, returning a count of the number of issues
     *
     * @param log The SBT Logger
-    * @param outputLocation The location of the Checkstyle report
+    * @param outputFile The Checkstyle report output path.
     * @param severityLevel The severity level at which to fail the build if style issues exist at that level
     * @return A count of the total number of issues processed
     */
-  private def processIssues(log: Logger, outputLocation: String, severityLevel: CheckstyleSeverityLevel): Int = {
-    val report = scala.xml.XML.loadFile(file(outputLocation))
-    val checkstyleSeverityLevelIndex = CheckstyleSeverityLevel.values.toArray.indexOf(severityLevel)
-    val appliedCheckstyleSeverityLevels = CheckstyleSeverityLevel.values.drop(checkstyleSeverityLevelIndex)
-
-
+  def processIssues(log: Logger, outputFile: File, severityLevel: CheckstyleSeverityLevel): Int = {
+    val report = XML.loadFile(outputFile)
     (report \ "file").flatMap { file =>
       (file \ "error").map { error =>
-        val severity = CheckstyleSeverityLevel.withName(error.attribute("severity").get.head.text)
-        appliedCheckstyleSeverityLevels.contains(severity) match {
-          case false => 0
-          case true => val lineNumber = error.attribute("line").get.head.text
-            val filename = file.attribute("name").get.head.text
-            val errorMessage = error.attribute("message").get.head.text
-            log.error("Checkstyle " + severity + " found in " + filename + ":" + lineNumber + ": " + errorMessage)
-            1
+        val severity = CheckstyleSeverityLevel.withName(error \@"severity")
+        if (severityLevel > severity) {
+          0
+        } else {
+          val lineNumber = error \@ "line"
+          val filename = file \@ "name"
+          val errorMessage = error \@ "message"
+          log.error(s"Checkstyle $severity found in $filename:$lineNumber: $errorMessage")
+          1
         }
       }
     }.sum
@@ -106,7 +47,7 @@ object Checkstyle {
     * @param input The XML file produced by checkstyle
     * @param transformations The XSLT transformations to be applied
     */
-  private def applyXSLT(input: File, transformations: Set[CheckstyleXSLTSettings]): Unit = {
+  def applyXSLT(input: File, transformations: Set[CheckstyleXSLTSettings]): Unit = {
     val processor = new Processor(false)
     val source = processor.newDocumentBuilder().build(input)
 
@@ -120,27 +61,6 @@ object Checkstyle {
       transformer.transform()
       transformer.close()
       output.close()
-    }
-  }
-
-  /**
-    * Wraps a block of code and executes it, preventing exits from the
-    * JVM.  It does this by using a custom SecurityManager that throws
-    * an exception if exitVM permission is checked.
-    *
-    * @param block The block of code to wrap and execute
-    */
-  def noExit(block: => Unit): Unit = {
-    val original = System.getSecurityManager
-    System.setSecurityManager(new NoExitSecurityManager())
-
-    try {
-      block
-    } catch {
-      case _: NoExitException =>
-      case e: Throwable => throw e
-    } finally {
-      System.setSecurityManager(original)
     }
   }
 }
--- src/main/scala/com/etsy/sbt/checkstyle/CheckstylePlugin.scala
+++ src/main/scala/com/etsy/sbt/checkstyle/CheckstylePlugin.scala
@@ -1,9 +1,12 @@
 package com.etsy.sbt.checkstyle
 
+import java.net.URLClassLoader
+
 import com.etsy.sbt.checkstyle.CheckstyleSeverityLevel.CheckstyleSeverityLevel
 import sbt.Def.Initialize
 import sbt.Keys._
 import sbt._
+import sbt.io.Path
 import sbt.util.NoJsonWriter
 
 /**
@@ -22,6 +25,8 @@ object CheckstylePlugin extends AutoPlugin {
     private[this] def SettingKey[T: Manifest](label: String, description: String = "") =
       sbt.SettingKey[T](label, description)(implicitly, NoJsonWriter())
     // scalastyle:on method.name
+
+    val CheckstyleLibs = config("CheckstyleLibs")
 
     val checkstyle = TaskKey[Unit]("checkstyle", "Runs checkstyle")
     val checkstyleOutputFile = SettingKey[File]("checkstyle-target", "The location of the generated checkstyle report")
@@ -43,28 +48,77 @@ object CheckstylePlugin extends AutoPlugin {
       * @param conf The configuration (Compile or Test) in which context to execute the checkstyle command
       */
     def checkstyleTask(conf: Configuration): Initialize[Task[Unit]] = Def.task {
-      Checkstyle.checkstyle(
-        (javaSource in conf).value,
-        (checkstyleOutputFile in conf).value,
-        checkstyleHeaderLocation.value,
-        (checkstyleConfigLocation in conf).value.getAbsolutePath,
-        (checkstyleXsltTransformations in conf).value,
-        (checkstyleSeverityLevel in conf).value,
-        streams.value
+      val log = (conf / checkstyle / streams).value.log
+
+      val headerFileOpt = {
+        val f = checkstyleHeaderLocation.value
+        if (f.getName == "") {
+          None
+        } else if (f.exists()) {
+          Some("checkstyle.header.file" -> f.getAbsolutePath)
+        } else {
+          log.warn(s"checkstyleHeaderLocation file not found: $f")
+          None
+        }
+      }
+
+      val forkOpts = (checkstyle / forkOptions).value
+      val trap = (checkstyle / trapExit).value
+      val r: ScalaRun = if ((checkstyle / fork).value) {
+        val newJVMOpts = forkOpts.runJVMOptions ++ headerFileOpt.map { case (k, v) => s"-D$k=$v" }
+        new ForkRun(forkOpts.withRunJVMOptions(newJVMOpts))
+      } else {
+        headerFileOpt foreach { case (k, v) => sys.props(k) = v }
+        new Run(clsLoader, trap)
+      }
+
+      val outputFile = (conf / checkstyleOutputFile).value
+      val checkstyleOpts = Seq(
+        "-c", (conf / checkstyleConfigLocation).value.getAbsolutePath, // checkstyle configuration file
+        "-f", "xml", // output format
+        "-o", outputFile.absolutePath, // output file
+        (conf / javaSource).value.absolutePath, // location of Java source file
       )
+
+      r.run(
+        "com.puppycrawl.tools.checkstyle.Main",
+        (CheckstyleLibs / managedClasspath).value.files,
+        checkstyleOpts,
+        Logger.Null
+      )
+
+      if (outputFile.exists) {
+        (conf / checkstyleXsltTransformations).value.foreach { xslt =>
+          Checkstyle.applyXSLT(outputFile, xslt)
+        }
+
+        (conf / checkstyleSeverityLevel).value.map { severityLevel =>
+          Checkstyle.processIssues(log, outputFile, severityLevel)
+        } match {
+          case Some(issuesFound) if issuesFound > 0 =>
+            log.error(s"$issuesFound issue(s) found in Checkstyle report: $outputFile")
+          case _ =>
+            log.info(s"Checkstyle success")
+        }
+      }
     }
+    private[this] def clsLoader(paths: Seq[File]) = new URLClassLoader(Path.toURLs(paths), ClassLoader.getSystemClassLoader)
   }
 
   // scalastyle:off import.grouping
   import autoImport._
   // scalastyle:on import.grouping
 
-  private lazy val commonSettings: Seq[Def.Setting[_]] = Seq(
-    checkstyleXsltTransformations := None,
-    checkstyleSeverityLevel := None
-  )
+  override def projectConfigurations = Seq(CheckstyleLibs)
 
   override lazy val projectSettings: Seq[Def.Setting[_]] = Seq(
+    checkstyleXsltTransformations := None,
+    checkstyleSeverityLevel := Some(CheckstyleSeverityLevel.Error),
+    libraryDependencies += "com.puppycrawl.tools"     % "checkstyle"   % "8.29" % CheckstyleLibs,
+    CheckstyleLibs / managedClasspath := Classpaths.managedJars(CheckstyleLibs, classpathTypes.value, update.value),
+    checkstyle / fork := false,
+    checkstyle / forkOptions := (Compile / forkOptions).value,
+    checkstyle / trapExit := true,
     checkstyleOutputFile := target.value / "checkstyle-report.xml",
     checkstyleOutputFile in Test := target.value / "checkstyle-test-report.xml",
     checkstyleHeaderLocation := file(""), // default `file("")` means checkstyleHeaderLocation will not be used
@@ -72,5 +126,5 @@ object CheckstylePlugin extends AutoPlugin {
     checkstyleConfigLocation in Test := checkstyleConfigLocation.value,
     checkstyle := checkstyleTask(Compile).value,
     checkstyle in Test := checkstyleTask(Test).value
-  ) ++ commonSettings
+  )
 }
